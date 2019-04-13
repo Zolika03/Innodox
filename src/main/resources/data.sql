259309de63e5769305029ec0672564f92582ff95
Zolika03 <dszotyi@gmail.com>
2019-04-14 00:46:51 +0200

    DB init data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO USER (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVE) 
+			VALUES ('Elek', 'Teszt', 'elek.teszt@tesz.hu', 'alma', 1);
+INSERT INTO USER (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVE) 
+			VALUES ('Eszter', 'Vincs', 'eszter.vincs@gmal.hu', 'alma', 1);
+
+
+INSERT INTO BOOK (TITLE,AUTHORS,PUBLISHER,CATEGORY,PREFACE,CONTENT,QUANTITY) 
+			VALUES ('The Temp','Michelle Frances','21. Század Kiadó','pszichológiai thriller',
+					'Carrie sikeres tévéproducer, a férje egyszerűen tökéletes – csodálatos életet élnek. De amikor a nő teherbe esik, a gondosan felépített világuk hirtelen darabokra hullik.',
+					'– A nyolc a szerencseszámod – súgta oda Carrie Adrian- nek, ügyelve rá, hogy eltakarja a száját a rájuk szegezôdô televíziós kamera elôl. Arcán higgadt, semleges kifejezés látszott, mialatt a kivetítôn a legjobb forgatókönyv dí-jára jelölt alkotásokból játszottak be részleteket. Élôben közvetítették a mûsort, olyankor sohasem lehetett tudni, mikor vágja be az arcát a rendezô.Adrian nem fordult felé.– És?– A nyolcadik sorban ülünk.A férfi lepillantott az elôttük lévô ülésekre: a brit té-véipar krémje töltötte meg a Royal Albert Hall székso-rait. Carrie látta rajta, hogy fejben számolgat, és ô is így tett. Ösztönösen ugrott a pillantásuk az elôttük – az ô helyükön! – ülôkre. Egy igen sikeres krimisorozat kere-sett szerzôjére, oldalán a kíméletlen, ámbátor elbûvölô gyilkost alakító fôszereplôvel.',
+					'2');
+					
+INSERT INTO BOOK (TITLE,AUTHORS,PUBLISHER,CATEGORY,PREFACE,CONTENT,QUANTITY) 
+			VALUES ('Good friend','Teresa Driscoll','21. Század Kiadó','pszichológiai thriller',
+					'Sophie vonaton ül a férjével, messze otthonától és négyéves kisfiától, Bentől. Rémisztő telefonhívást kap. Tragikus baleset történt, két kisfiú kórházba került. Az egyikük Ben.',
+					'A vonat mosdójában állok – terpeszben –, és a kicsi, rozs-damentesacél mosdó fölé hajolok, miközben igyekszem... csak... levegőt... venni... a francba... és próbálok rájönni, ugyan, mi a fene köze van ennek az egésznek a vörösbegy-hez. Több mint 350 kilométerrel odébb a gyerekemet vad-idegenek ápolják egy kórházban. Lehet, hogy már kivették a lépét.',
+					'3');
+										
+INSERT INTO BOOK (TITLE,AUTHORS,PUBLISHER,CATEGORY,PREFACE,CONTENT,QUANTITY) 
+			VALUES ('Egy év a konyhámban','Mautner Zsófi','Strucc Könyvkiadó','életvitel',
+					'Ahogy változnak az évszakok és velük együtt a rendelkezésre álló alapanyagok, úgy változik a kulináris hangulatunk, és az is, hogy milyen ételeket kívánunk. Vannak azonban receptek.',
+					'Megannyi blogbejegyzés, újságcikk és szakácskönyv után Mautner Zsófi ezúttal az életre szóló, kedvenc házi receptjeit rendezte kötetbe. Sok száz étele közül ezek alapján főz a leggyakrabban magának, családjának és barátainak, mostantól pedig Ön is megismerheti ezeket a személyesen kipróbált és bevált recepteket.',
+					'10');
