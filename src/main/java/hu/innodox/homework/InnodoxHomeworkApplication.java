b7a9ae92ed30df1a5665a10f0afe31b72b599e88
Zolika03 <dszotyi@gmail.com>
2019-04-13 18:06:45 +0200

    This is where it all begins...

---
+++ src/main/java/hu/innodox/homework/InnodoxHomeworkApplication.java
@@ -0,0 +1,13 @@
+package hu.innodox.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnodoxHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnodoxHomeworkApplication.class, args);
+	}
+
+}
